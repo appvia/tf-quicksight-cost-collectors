2a0b54018ff7c7fcca02fbdd4c07c536f895ea5c
George Taylor <george.taylor@appvia.io>
2025-06-03 17:24:45 +0100

    chore: remove unused variables from user data collector module

--- modules/user-data-collector/variables.tf
+++ modules/user-data-collector/variables.tf
@@ -4,47 +4,6 @@ variable "tags" {
   default     = {}
 }
 
-variable "usage_data_bucket_name" {
-  description = "Name of the S3 bucket where usage data is stored"
-  type        = string
-}
-
-variable "athena_workgroup_name" {
-  description = "Name of the Athena workgroup from the shared module"
-  type        = string
-}
-
-variable "athena_database_name" {
-  description = "Name of the Athena database from the shared module"
-  type        = string
-}
-
-variable "create_quicksight_data_set" {
-  description = "Whether to create a Quicksight data set"
-  type        = bool
-  default     = true
-}
-
-variable "quicksight_data_source_arn" {
-  description = "ARN of the Quicksight data source"
-  type        = string
-}
-
-variable "quicksight_data_set_permissions" {
-  description = "Permissions for the Quicksight data set"
-  type = list(object({
-    principal = string
-    actions   = list(string)
-  }))
-  default = []
-}
-
-
-variable "usage_data_bucket_key_arn" {
-  description = "ARN of the KMS key for the usage data bucket"
-  type        = string
-}
-
 variable "mock_mode" {
   description = "Enable mock mode for testing the lambda function"
   type        = bool
